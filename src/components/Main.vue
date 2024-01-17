1d333f94db995d74176ecff16bf9ae9c5f55e43a
zGiusee <giusebelforte@gmail.com>
2024-01-17 16:13:01 +0100

    [UPDATE] added code base in main.vue

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+<template>
+    <div>
+
+    </div>
+</template>
+<style lang="scss">
+@use '../styles/generals.scss' as *;
+</style>
