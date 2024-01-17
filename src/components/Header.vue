0954760d61607250d89be882c1f837ae9bb91429
zGiusee <giusebelforte@gmail.com>
2024-01-17 17:37:34 +0100

    [ADD] Header nav-links array

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,114 @@
 export default {
     data() {
         return {
+            headerLinks: [
+                {
+                    linkName: 'Home',
+                    linkRef: '#',
+                },
+                {
+                    linkName: 'Blog',
+                    linkRef: '#',
+                },
+                {
+                    linkName: 'Events',
+                    linkRef: '#',
+                    linkSection: [
+                        {
+                            linkSectionName: 'Choral Music',
+                            linkSectionRef: '#',
+                        },
+                        {
+                            linkSectionName: 'Concert Band',
+                            linkSectionRef: '#',
+                        },
+                        {
+                            linkSectionName: 'Opera Concerts',
+                            linkSectionRef: '#',
+                        },
+                        {
+                            linkSectionName: 'Symphony Orchestra',
+                            linkSectionRef: '#',
+                        },
+                        {
+                            linkSectionName: 'Family Concerts',
+                            linkSectionRef: '#',
+                        },
+
+                    ],
+                },
+                {
+                    linkName: 'Gallery',
+                    linkRef: '#',
+                },
+                {
+                    linkName: 'About Us',
+                    linkRef: '#',
+                },
+                {
+                    linkName: 'Conctact Us',
+                    linkRef: '#',
+                },
+                {
+                    linkName: 'Shop',
+                    linkRef: '#',
+                    linkSection: [
+                        {
+                            linkSectionName: 'Product Type',
+                            linkSectionRef: '#',
+                            linkSectionTypes: [
+                                {
+                                    typesName: 'Simple Product',
+                                    typesRef: '#',
+                                },
+                                {
+                                    typesName: 'Exsternal/Affiliate Product',
+                                    typesRef: '#',
+                                },
+                                {
+                                    typesName: 'Downloadable Product',
+                                    typesRef: '#',
+                                },
+                                {
+                                    typesName: 'Group Product',
+                                    typesRef: '#',
+                                },
+                                {
+                                    typesName: 'In.Stock Product',
+                                    typesRef: '#',
+                                },
+                                {
+                                    typesName: 'Variable Product',
+                                    typesRef: '#',
+                                },
+                            ],
+                        },
+                        {
+                            linkSectionName: 'Shop Page',
+                            linkSectionRef: '#',
+                            linkSectionTypes: [
+                                {
+                                    typesName: 'Checkout',
+                                    typesRef: '#',
+                                },
+                                {
+                                    typesName: 'Cart',
+                                    typesRef: '#',
+                                },
+                                {
+                                    typesName: 'Downloads',
+                                    typesRef: '#',
+                                },
+                                {
+                                    typesName: 'My Account',
+                                    typesRef: '#',
+                                },
+                            ],
+                        },
+
+                    ],
+                },
+            ],
 
         }
     },
@@ -11,22 +119,44 @@ export default {
 }
 </script>
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-6">
-                <div>
+    <header class="container-fluid bg-black">
+        <div class="row h-100 align-items-center">
 
+            <div class="col-6">
+                <div class="logo-container">
+                    <img src="../assets/img/Logo.png" alt="">
                 </div>
             </div>
-            <div class="col-6">
-                <div>
 
+            <div class="col-6">
+                <div class="nav-list-container">
+                    <ul>
+                        <li></li>
+                    </ul>
                 </div>
             </div>
+
         </div>
-    </div>
+    </header>
 </template>
 <style lang="scss">
 @use '../styles/generals.scss' as *;
 @use '../styles/partials/variables' as *;
+
+header {
+    height: 102px;
+    position: fixed;
+    z-index: 999;
+
+    .logo-container {
+        margin-left: 25px;
+
+        img {
+            width: 170px;
+        }
+    }
+
+
+
+}
 </style>
