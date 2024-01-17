9a87319d1f6fa393d82e34f2a874d378dfdf7642
zGiusee <giusebelforte@gmail.com>
2024-01-17 17:04:28 +0100

    [ADD] added header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,7 +14,9 @@ export default {
     <div class="container-fluid">
         <div class="row">
             <div class="col-6">
+                <div>
 
+                </div>
             </div>
             <div class="col-6">
 
