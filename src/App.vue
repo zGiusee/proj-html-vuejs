4cd86687b64081cf3b6b3f9a46dd82e20963a43a
moonymel <musumeci.me@gmail.com>
2024-01-17 17:07:17 +0100

    [UPDATE] prova

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,7 @@ export default {
 
 <template>
     <div>
-        <div>
-            ciao
-        </div>
+
     </div>
 </template>
 
---
+++ src/components/main_components/WhoWeAreDue.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
