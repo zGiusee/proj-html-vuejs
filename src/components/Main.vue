41567b1b6f199bd3ec23bb01336744a3c81edf94
zGiusee <giusebelforte@gmail.com>
2024-01-18 11:17:56 +0100

    [ADD] added jumbotron slider function and array

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,17 @@
 <script>
 import AppOutNow from './main_components/AppOutNow.vue';
+import JumbotronSlider from './main_components/JumbotronSlider.vue';
 
 export default {
     components: {
         AppOutNow,
+        JumbotronSlider,
     }
 }
 </script>
 <template>
     <div>
+        <JumbotronSlider></JumbotronSlider>
         <AppOutNow></AppOutNow>
     </div>
 </template>
---
+++ src/components/main_components/JumbotronSlider.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+    name: 'JumbotronSlider',
+    data() {
+        return {
+            currentImage: 0,
+            slides: [
+                {
+                    img: '../../assets/image1.png',
+                    title: 'Music in this video',
+                    subtitle: 'Instrumental rock',
+                },
+                {
+                    img: '../../assets/image2.png',
+                    title: 'Music of the spirits',
+                    subtitle: 'Instrumental rock',
+                },
+            ]
+
+        }
+    },
+    methods: {
+        nextImg() {
+
+            if (this.currentImage == this.slides.length - 1) {
+                this.currentImage = 0;
+            }
+            else {
+                this.currentImage++;
+            }
+        },
+        prevImg() {
+
+            if (this.currentImage == 0) {
+                this.currentImage = this.slides.length - 1;
+            }
+            else {
+                this.currentImage--;
+            }
+        },
+        getImagePath: function (imgPath) {
+            let url = new URL(imgPath, import.meta.url).href;
+            return url
+        }
+    },
+
+}
+</script>
+<template>
+    <div>
+        <img :src="`${getImagePath(slides[this.currentImage].img)}`" alt="">
+    </div>
+</template>
+<style lang="scss">
+@use '../../styles/generals.scss' as *;
+@use '../../styles/partials/variables' as *;
+
+img {
+    width: 100%;
+}
+</style>
