11e868ad8630db2083e50a3beaf590d8853dad22
zGiusee <giusebelforte@gmail.com>
2024-01-18 18:17:34 +0100

    [UPDATE] Dynamic header done only stylization remaining

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,6 +17,7 @@ export default {
                     linkName: 'Events',
                     linkRef: '#',
                     active: false,
+                    linkSectionDropDownEvents: true,
                     linkSection: [
                         {
                             linkSectionName: 'Choral Music',
@@ -60,10 +61,13 @@ export default {
                     linkName: 'Shop',
                     linkRef: '#',
                     active: false,
+                    linkSectionDropDownShop: true,
                     linkSection: [
                         {
                             linkSectionName: 'Product Type',
+                            id: 'ProductType',
                             linkSectionRef: '#',
+                            linkSectionTypesDropDownProduct: true,
                             linkSectionTypes: [
                                 {
                                     typesName: 'Simple Product',
@@ -93,8 +97,10 @@ export default {
                         },
                         {
                             linkSectionName: 'Shop Page',
+                            id: 'ShopPage',
                             linkSectionRef: '#',
                             active: false,
+                            linkSectionTypesDropDownShop: true,
                             linkSectionTypes: [
                                 {
                                     typesName: 'Checkout',
@@ -141,12 +147,42 @@ export default {
             </div>
 
             <!-- NAV LIST AND SEARCH BOX -->
-            <div class="col-6">
-                <div class="nav-list-container position-relative ">
+            <div class="col-6 h-100">
+                <div class="nav-list-container h-100">
 
                     <ul class="link_list">
                         <li v-for="link, index in headerLinks" :key="index" :href="link.linkRef"
-                            :class="link.active ? 'active' : ''" class="link_list_li">{{ link.linkName }}
+                            :class="link.active ? 'active' : ''" class="link_list_li" :id="link.linkName">{{ link.linkName
+                            }}
+
+                            <ul class="link_section_list" v-if="link.linkSectionDropDownEvents">
+                                <li v-for="section_url, index in link.linkSection" :key="index">
+                                    {{ section_url.linkSectionName }}
+                                </li>
+                            </ul>
+
+                            <ul class="link_section_list_right" v-if="link.linkSectionDropDownShop">
+                                <li class="link_section_list_li" v-for="section_url, index in link.linkSection" :key="index"
+                                    :id="section_url.id">
+                                    {{ section_url.linkSectionName }}
+
+                                    <ul class="link_types_list_shop" v-if="section_url.linkSectionTypesDropDownShop">
+                                        <li v-for="types_url, index in section_url.linkSectionTypes" :key="index">
+                                            {{ types_url.typesName }}
+                                        </li>
+                                    </ul>
+
+                                    <ul class="link_types_list_product" v-if="section_url.linkSectionTypesDropDownProduct">
+                                        <li v-for="types_url, index in section_url.linkSectionTypes" :key="index">
+                                            {{ types_url.typesName }}
+                                        </li>
+                                    </ul>
+
+                                </li>
+                            </ul>
+
+
+
                         </li>
                     </ul>
 
@@ -165,12 +201,53 @@ export default {
 @use '../styles/generals.scss' as *;
 @use '../styles/partials/variables' as *;
 
+#ProductType:hover .link_types_list_product {
+    display: block;
+}
+
+.link_types_list_product {
+    display: block;
+}
+
+
+
+#ShopPage:hover .link_types_list_shop {
+    display: block;
+}
+
+.link_types_list_shop {
+    display: block;
+}
+
+
+
+
+#Shop:hover .link_section_list_right {
+    display: block;
+}
+
+.link_section_list_right:hover {
+    display: block;
+}
+
+#Events:hover .link_section_list {
+    display: block;
+}
+
+.link_section_list:hover {
+    display: block;
+}
+
+.link_types_list_product li:hover {
+    color: $main_color;
+}
+
 // HEADER STYLES
 header {
-    height: 102px;
     position: fixed;
     z-index: 999;
     text-transform: uppercase;
+    height: 102px;
 
     .logo-container {
 
@@ -179,16 +256,22 @@ header {
         }
     }
 
+    // NAV BAR LIST MENU
     .nav-list-container {
         display: flex;
         justify-content: end;
+        position: relative;
 
         .link_list {
             display: flex;
             list-style-type: none;
+            padding: 0px;
             margin: 0px;
+            height: 100%;
 
             .link_list_li {
+                display: flex;
+                align-items: center;
                 color: white;
                 font-size: 15px;
                 letter-spacing: 0.5px;
@@ -197,33 +280,88 @@ header {
                 cursor: pointer;
                 transition: 0.3s;
 
+
                 &.active {
                     color: $main_color;
                 }
-
-                &:hover {
-                    color: $main_color;
-                }
-
-                // .link_section_list {
-                //     list-style-type: none;
-                //     position: absolute;
-                //     padding: 0px;
-                //     background-color: $main_grey;
-
-                //     .link_section_list_li {
-                //         padding: 15px 10px 15px 10px;
-                //         border-bottom: 0.5px solid white;
-                //     }
-                // }
             }
 
-
         }
+
+
     }
 
 }
 
+
+// SECTION LIST
+.link_section_list {
+    list-style-type: none;
+    position: absolute;
+    top: 102px;
+    padding: 0px;
+    background-color: $main_grey;
+    display: none;
+
+    li {
+        width: 370px;
+        padding: 15px 5px;
+        border-bottom: 0.5px solid white;
+
+    }
+}
+
+.link_section_list_right {
+    list-style-type: none;
+    position: absolute;
+    top: 102px;
+    right: 10px;
+    width: 370px;
+    padding: 0px;
+    display: none;
+    background-color: $main_grey;
+
+    .link_section_list_li {
+        padding: 15px 5px;
+        border-bottom: 0.5px solid white;
+    }
+}
+
+// TYPE LIST
+.link_types_list_product {
+    list-style-type: none;
+    position: absolute;
+    top: 0px;
+    right: 370px;
+    padding: 0px;
+    background-color: $main_grey;
+    display: none;
+
+    li {
+        width: 370px;
+        padding: 15px 5px;
+        border-bottom: 0.5px solid white;
+    }
+}
+
+.link_types_list_shop {
+    list-style-type: none;
+    position: absolute;
+    padding: 0px;
+    top: 53px;
+    right: 370px;
+    display: none;
+    background-color: $main_grey;
+
+    li {
+        width: 370px;
+        padding: 15px 5px;
+        border-bottom: 0.5px solid white;
+    }
+}
+
+
+// SEARCH ICON
 .search-container {
     padding: 1.5rem;
 
@@ -232,7 +370,7 @@ header {
     }
 }
 
-
+// GENERAL POSITIONING
 .my-padding-x {
     padding: 0rem 2rem;
 }
