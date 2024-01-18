c039f7e930ee84146dd5ab0ed4b0846ecfc02a75
moonymel <musumeci.me@gmail.com>
2024-01-18 18:15:29 +0100

    [UPDATE] fixed slider in BestMusicBlog.vue, styled and completed BestMusivBlog.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -55,36 +55,43 @@ export default {
                 {
                     title: 'Hip Hop wired to dance make',
                     date: '06 November 2021',
+                    img: '../src/assets/img/hip_hop_wired_to_dance_make.jpg',
                     content: 'Driving short distances music is a music genre that includes traditional folk music and the contemporary genre that evolved from the former',
                 },
                 {
                     title: 'Music business worldwide',
                     date: '27 May 2022',
+                    img: '../src/assets/img/music_business-worldwide.jpg',
                     content: 'Accelerate work and drive productivity The Beatles experimentation and creative freedom led other bands to experiment in various ways as well.',
                 },
                 {
                     title: 'Giveaways rock to all',
                     date: '07 May 2022',
+                    img: '../src/assets/img/giveaways_rock.jpg',
                     content: 'Signs Your Car Battery Has To Be Replaced When the dashboard lights start flashing, this is a sign the battery is dying. Several issues arise and',
                 },
                 {
                     title: 'Drowned in sound feel joy',
                     date: '10 April 2022',
+                    img: '../src/assets/img/drowned_in_sound_feel_joy.jpg',
                     content: 'Different Types of Music Content For most music creators, they take their work very seriously because of the emotional attachment they may harbor',
                 },
                 {
                     title: 'Dancing astronaut bounce ipsum',
                     date: '10 April 2022',
+                    img: '../src/assets/img/dancing_astronaut_bounce_ipsum.jpg',
                     content: 'Music Promotion Channels There are really a lot of music promotion channels out there. Being a music promotion channel involves a lot of work and',
                 },
                 {
                     title: 'Consequence of sound make us',
                     date: '14 February 2022',
+                    img: '../src/assets/img/consequence_of_sound_make_us.jpg',
                     content: 'Electronic Dance Music Rhythm and Blues Before this, R &amp; B was known as ‘race music,’ a term that originated in the African American',
                 },
                 {
                     title: 'Artist development and production',
                     date: '07 December 2021',
+                    img: '../src/assets/img/artist_development_and_production.jpg',
                     content: 'Characterised by its own beat patterns, the lyrics feature what is often extreme violence snd talk of criminal acts. As a result, theres been',
                 },
             ],
--- src/components/main_components/BestMusicBlog.vue
+++ src/components/main_components/BestMusicBlog.vue
@@ -1,16 +1,177 @@
 <script>
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+
+
 export default {
     name: 'BestMusicBlog',
     props: {
         bmb_cards: Array,
-    }
+    },
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation,
+    },
+
+    methods: {
+        next() {
+            this.$refs.carousel.next()
+        },
+        prev() {
+            this.$refs.carousel.prev()
+        },
+    },
+
 }
 </script>
 
 <template>
-    <div>
+    <div class="bmb-cont">
+        <div class="container">
+            <!-- TITOLI MAIN DELLA SEZIONE -->
+            <div class="text-center">
+                <span class="bmb-subtitle">music blog</span><br>
+                <span class="bmb-title">best music blog</span>
+            </div>
+            <div class="slider-cont">
+                <!-- SLIDER BLOG -->
+                <div class="slider">
+                    <carousel ref="carousel" :items-to-show="3">
+                        <slide v-for="slide in bmb_cards" :key="slide">
+                            <div class="bmb-card my-3">
+                                <div class="card-image">
+                                    <img :src="slide.img">
+                                </div>
+                                <div class="card-title">{{ slide.title }}</div>
+                                <div class="card-date"><i class="bi bi-calendar3"></i> {{ slide.date }}</div>
+                                <div class="card-content">{{ slide.content }}</div>
+                            </div>
+                        </slide>
+                    </carousel>
+                </div>
+                <!-- FRECCETTE SLIDER  -->
+                <div class="arrows">
+                    <div class="left-arrow" @click="prev">
+                        <i class="bi bi-chevron-left"></i>
+                    </div>
+                    <div class="right-arrow" @click="next">
+                        <i class="bi bi-chevron-right"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use '../../styles/generals.scss' as *;
+@use '../../styles/partials/variables.scss' as *;
+@use '../../styles/partials/mixins.scss' as *;
+
+.bmb-cont {
+    background-color: black;
+    color: rgb(165, 165, 165);
+    padding: 100px 0;
+
+    .bmb-subtitle {
+        @include orange_title;
+    }
+
+    .bmb-title {
+        @include white_title;
+    }
+
+    .slider-cont {
+        display: flex;
+        flex-wrap: wrap;
+        position: relative;
+
+        .slider {
+            z-index: 3;
+        }
+
+        &:hover .arrows {
+            transition: 1s ease-out;
+            color: white;
+
+            .left-arrow,
+            .right-arrow {
+                cursor: pointer;
+            }
+        }
+
+        .bmb-card {
+
+            width: calc(100% - 20px);
+            margin: 0 10px;
+            text-align: left;
+
+            .card-title {
+                @include min_title
+            }
+
+            .card-title:hover {
+                color: $main_color;
+            }
+
+            .card-content {
+                @include min_content
+            }
+
+            .card-date {
+                @include min_date
+            }
+
+            .card-image {
+                overflow: hidden;
+
+                img {
+                    transition: transform 0.5s ease;
+                    width: 100%;
+                }
+
+                &:hover img {
+                    transform: scale(1.05);
+                }
+            }
+
+            .bmb-icons {
+                filter: brightness(0) invert(1);
+                height: 15px;
+                margin: 0px 8px;
+
+            }
+        }
+
+        .arrows {
+            width: calc(100% + 80px);
+            height: 100%;
+            color: black;
+            font-size: 40px;
+            position: absolute;
+            top: -40px;
+            left: -40px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            z-index: 0;
+
+
+            &:hover {
+                transition: 1s ease-out;
+                color: white;
+
+                .left-arrow,
+                .right-arrow {
+                    cursor: pointer;
+                }
+            }
+        }
+    }
+
+
+}
+</style>
