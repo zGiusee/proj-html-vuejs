6b3658d8871bfd1122de72c372dd5912f2b579eb
zGiusee <giusebelforte@gmail.com>
2024-01-19 12:48:49 +0100

    [UPDATE] Fixed scrollbar problem with sense the jazz 1

--- src/components/main_components/sense_the_jazz_1.vue
+++ src/components/main_components/sense_the_jazz_1.vue
@@ -4,57 +4,77 @@ export default {
 }
 </script>
 
-<template lang="">
-<div class="main-container">
-    <div class="bg-black ">
-        <div class="px-4 py-5  text-center text-white"> 
-            <div class="wwr-subtitle">
-               <h6>WHO WE ARE</h6>
-            </div>
-            <div class="wwr-title">
-            SENSE THE JAZZ
-            </div>
-            
-            <div class="col-lg-6 mx-auto">
-                <div class="text-p mb-5">
-                    <span class="fs-6">The Story Of Jazz And Blues Society Is The Story Of Determination, Passion, Idealism,
-                        Community, And Of Course, Music. Founded In 1985 And Incorporated In 1986, As A Community Based,
-                        Not-For-Profit, Charitable Arts Organization Located In Vancouver, British Columbia, The Society, From
-                        The Very Beginning, Has Worked Diligently To Establish Vancouver As A Centre For The Creation And
-                        Exchange Of Sounds And Ideas Between The Local, National And International Music Communities.
-                    </span>
+<template>
+    <div class="bg-black">
+        <div class="main-container ">
+            <div class="bg-black ">
+                <div class="px-4 py-5  text-center text-white">
+                    <div class="row">
+
+                        <div class="wwr-subtitle col-12">
+                            <h6>WHO WE ARE</h6>
+                        </div>
+                        <div class="wwr-title col-12">
+                            SENSE THE JAZZ
+                        </div>
+
+                        <div class="col-12 justify-content-center d-flex ">
+                            <div class="text-p mb-5 my-p">
+                                <span class="fs-6">The Story Of Jazz And Blues Society Is The Story Of Determination,
+                                    Passion,
+                                    Idealism,
+                                    Community, And Of Course, Music. Founded In 1985 And Incorporated In 1986, As A
+                                    Community
+                                    Based,
+                                    Not-For-Profit, Charitable Arts Organization Located In Vancouver, British Columbia, The
+                                    Society, From
+                                    The Very Beginning, Has Worked Diligently To Establish Vancouver As A Centre For The
+                                    Creation And
+                                    Exchange Of Sounds And Ideas Between The Local, National And International Music
+                                    Communities.
+                                </span>
+                            </div>
+
+                        </div>
+
+                        <div class="col-12">
+                            <div class="d-gap-2 d-sm-flex justify-content-sm-center">
+                                <button type="button"
+                                    class="btn btn-outline-custom-orange btn-custom px-5 py-2 text-white">READ
+                                    MORE</button>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-                
-                 <div class="d-gap-2 d-sm-flex justify-content-sm-center">
-                    <button type="button" class="btn btn-outline-custom-orange btn-custom px-5 py-2 text-white">READ MORE</button>
-                 </div>
-               
             </div>
-        </div>
-    </div>
 
-    <div class="bg-img section-size">
-      <div class="row">
-        <div class="col-12 mt-custom">
-            <div class="player d-flex justify-content-center ">
-                <i class="bi bi-play-circle pointer"></i>
-            </div>
-            <div class="wwr-title d-flex justify-content-center ">
-                NEW MUSIC VIDEO
+            <div class="bg-img section-size w-100">
+                <div class="row w-100 ">
+                    <div class="col-12 mt-custom">
+                        <div class="player d-flex justify-content-center ">
+                            <i class="bi bi-play-circle pointer"></i>
+                        </div>
+                        <div class="wwr-title d-flex justify-content-center ">
+                            NEW MUSIC VIDEO
 
+                        </div>
+
+                    </div>
+                </div>
             </div>
 
         </div>
-      </div>
     </div>
-
-</div>
-
 </template>
 <style lang="scss">
 @use '../../styles/generals.scss' as *;
 @use '../../styles/partials/variables.scss' as *;
 @use '../../styles/partials/mixins' as*;
+
+.my-p {
+    width: 840px;
+}
 
 .wwr-subtitle {
     @include orange_title
@@ -66,6 +86,7 @@ export default {
 
 .main-container {
     font-family: "Oswald", sans-serif;
+    width: 100%;
 }
 
 .btn-outline-custom-orange {
